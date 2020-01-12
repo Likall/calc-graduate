70688f65c7ebeae211acdb8c736abd90785fc2b6
LikaLL <1422507030@qq.com>
2020-01-12 09:12:24 +0800

    增加首页header

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
+import Index from '@/components/Index'
 Vue.use(Router)
 
 export default new Router({
@@ -14,6 +15,11 @@ export default new Router({
 		path: '/',
 		name: 'Login',
 		component: Login
+	},
+	{
+		path: '/index',
+		name: 'index',
+		component: Index
 	}
   ]
 })
